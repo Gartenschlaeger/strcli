d7945e67d16811aab4f7a4bb0e041a0dfba15eda
Kai Gartenschläger <mail@kaisnet.de>
2023-03-02 20:29:46 +0100

    remove size check

--- utilities/utilities.go
+++ utilities/utilities.go
@@ -10,7 +10,7 @@ func GetStandardInputString() string {
 
 	fi, _ := os.Stdin.Stat()
 
-	if fi.Size() > 0 && (fi.Mode()&os.ModeCharDevice) == 0 {
+	if (fi.Mode() & os.ModeCharDevice) == 0 {
 		bytes, _ := ioutil.ReadAll(os.Stdin)
 		str := string(bytes)
 		str = strings.Trim(str, "\n ")
